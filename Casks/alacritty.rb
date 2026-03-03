08d45db97d0ad4368bc684b21c994d32aa906650
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2026-03-03 17:49:44 +0100

    Comment out gatekeeper disable line
    Comment out the disable! line for gatekeeper check.

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -12,7 +12,7 @@ cask "alacritty" do
     strategy :github_latest
   end
 
-  disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  #disable! date: "2026-09-01", because: :fails_gatekeeper_check
 
   depends_on macos: ">= :big_sur"
 
